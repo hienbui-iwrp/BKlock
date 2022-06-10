2a080bc9349c01ad1a7fda57749ce9de08164068
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2022-06-11 00:04:25 +0700

    update filter product

--- Server/Controllers/filterProduct.php
+++ Server/Controllers/filterProduct.php
@@ -1,10 +1,10 @@
 <?php
-    // Get 
-    // http://localhost/controllers/getfeaturedproduct.php?price=[1,2,3]&branch=['Rolex']&sex=['Đồng hồ nam']&category=['Cơ - automatic']
+// Get 
+// http://localhost/controllers/filterproduct.php?price=[1,2,3]&branch=['Rolex']&sex=['Đồng hồ nam']&category=['Cơ - automatic']
 
-    include "../models/product.php";
-    include "./api.php";
-    $queries = array();
-    parse_str($_SERVER['QUERY_STRING'], $queries);
-    
-    sendResponse(200, json_encode(Product::filterProduct($queries["price"], $queries["branch"], $queries["sex"], $queries["category"])), "application/json");
+include "../models/product.php";
+include "./api.php";
+$queries = array();
+parse_str($_SERVER['QUERY_STRING'], $queries);
+
+sendResponse(200, json_encode(Product::filterProduct($queries["price"], $queries["branch"], $queries["sex"], $queries["category"])), "application/json");
