16a565fb22d348b652db996fa7a0a81766130414
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2022-06-12 16:14:04 +0700

    fix api

--- Server/Controllers/payment/make.php
+++ Server/Controllers/payment/make.php
@@ -1,23 +1,23 @@
 
 <?php
-    // Post
-    // http://localhost/controllers/payment/make.php
-    // body json:
-    // {
-    //     product: [{id: 1, quantity:10}],
-    //     userId: 1
-    // }
-    include "../../Models/Payment.php";
-    include "../api.php";
-    if($_SERVER["REQUEST_METHOD"] === "POST"){
-        $temp = json_decode(file_get_contents("php://input"));
+// Post
+// http://localhost/controllers/payment/make.php
+// body json:
+// {
+//     product: [{id: 1, quantity:10}],
+//     userId: 1
+// }
+include "../../Models/Payment.php";
+include "../api.php";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $temp = json_decode(file_get_contents("php://input"));
 
-        if (Payment::makePayment($temp->product, $temp->userId)) {
-            sendResponse(200, "success", "text/html");
-        } else {
-            sendResponse(200, "fail", "text/html");
-        }
+    if (Payment::makePayment($temp->product, $temp->userId)) {
+        sendResponse(200, "success", "text/html");
     } else {
-        sendResponse(405, "Method Not Allowed", "text/html");
+        sendResponse(200, "fail", "text/html");
     }
+} else {
+    sendResponse(405, "Method Not Allowed", "text/html");
+}
 ?>
