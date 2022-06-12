3caa5e20d55581954b6160b514e4a29915c6fde6
Hien_Nguyen <73571967+ngochienhv@users.noreply.github.com>
2022-06-12 15:11:38 +0700

    Update ui for sidebar and add button for update new

--- Client/src/component/admin/news.js
+++ Client/src/component/admin/news.js
@@ -1,14 +1,208 @@
-import React from 'react';
-import ProductCard from '../../component/general/productCard';
-import { Grid, Pagination, Text, Image, MediaQuery } from '@mantine/core';
-import Slider from '../general/slider';
-import FilterForm from '../../component/general/filterForm';
-import { useLocation } from 'react-router-dom';
-import { useWindowScroll } from '@mantine/hooks';
-import BreadCrumbs from '../general/breadCrumb';
+import React from "react";
+import {
+    Card,
+    Image,
+    Text,
+    Group,
+    Grid,
+    Pagination,
+    Space,
+    Spoiler,
+    Title,
+    Button
+} from "@mantine/core";
+import { Container, MediaQuery,TextInput,Box, Modal, Stack, Textarea } from '@mantine/core';
+import "../../css/news.css";
+import { useLocation } from "react-router-dom";
+import { useViewportSize } from "@mantine/hooks";
+import { Trash } from "tabler-icons-react";
 import "../../css/product.css";
 
+import { useForm } from '@mantine/form';
 
 export default function News() {
-    return <></>
+    const [opened, setOpened] = React.useState(false);
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const [activePage, setPage] = React.useState(1);
+    const maxItemPerPage = 6;
+    const total = Math.ceil(arr.length / maxItemPerPage);
+    let location = useLocation();
+    const { height, width } = useViewportSize();
+    return (
+        <>
+            {/* <Group position="center" style={{ paddingBottom: "2%", margin: "2% 5%", borderBottom: "1px solid #000" }}>
+                <Title order={1} >Quản Lý Tin Tức </Title>
+            </Group> */}
+            <Stack justify="space-around">
+            <Group position="center" style={{ paddingBottom: "2%", margin: "2% 5% 0", borderBottom: "1px solid #000" }}>
+                <Title order={1} >Quản lý Tin Tức </Title>
+            </Group>
+            <Group position="apart" direction="row" style={{ padding: "5px 5%" }}>
+                <Button radius="xl" onClick={() => setOpened(true)}>Thêm mới </Button>
+            </Group>
+        </Stack>
+            <div className="news">
+                <Grid style={{ margin: 0 }}>
+                    {arr
+                        .slice(
+                            (activePage - 1) * maxItemPerPage,
+                            activePage * maxItemPerPage
+                        )
+                        .map((x) => {
+                            return (
+                                <Grid.Col
+                                    xl={4}
+                                    lg={4}
+                                    md={6}
+                                    sm={6}
+                                    xs={12}
+                                    key={x}
+                                >
+                                    <Item />
+                                </Grid.Col>
+                            );
+                        })}
+                </Grid>
+                <Pagination
+                    onChange={(page) => {
+                        setPage(page);
+                        window.scrollTo({ top: 0, behavior: "smooth" });
+                    }}
+                    total={total}
+                    position="right"
+                    withEdges
+                    className="product-pagination"
+                />
+                ;
+            </div>
+            <Modal centered
+                    opened={opened}
+                    onClose={() => setOpened(false)}
+                    title="Thêm tin tức mới"
+                    size="lg"
+                >
+                    <Grid>
+                        <Grid.Col>
+                            <ProductDetail />
+                        </Grid.Col>
+                    </Grid>
+                </Modal>
+        </>
+    );
+}
+
+function Item() {
+    return (
+        <div
+            className="new-item"
+            style={{ width: "auto", marginLeft: 10, marginRight: 10 }}
+        >
+            <Card shadow="sm" p="lg">
+                <Card.Section>
+                    <Image
+                        src="https://bossluxurywatch.vn/uploads/san-pham/rolex/sky-dweller/rolex-sky-dweller-42mm-326938-0005.png"
+                        height={300}
+                        alt="watch"
+                        className="product-img-zoom"
+                    />
+                </Card.Section>
+                <Group position="apart" spacing="xs">
+                    <Text weight={700} size="xl" className="news__title">
+                        Sales
+                    </Text>
+                    <Button leftIcon={<Trash />}
+                        variant="outline" color="red"
+                        className="product-card-btn admin__delete-btn"
+                    >
+                        Xóa
+                    </Button>
+                </Group>
+
+                <Group direction="row" style={{ marginTop: 20 }}>
+                    <Text weight={500}>6/1/2022</Text>
+                    <Space w="xs" />
+                    <Text weight={500}>100 views</Text>
+                    <Space w="xs" />
+                    <Text weight={500}>100 likes</Text>
+                </Group>
+                <Spoiler maxHeight={100} showLabel="Show more" hideLabel="Hide">
+                    <Text weight={400} className="news__content">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry's standard dummy text ever since the 1500s,
+                        when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the release of
+                        Letraset sheets containing Lorem Ipsum passages, and
+                        more recently with desktop publishing software like
+                        Aldus PageMaker including versions of Lorem Ipsum.
+                    </Text>
+                </Spoiler>
+            </Card>
+        </div>
+    );
+}
+
+function ProductDetail() {
+    const { height, width } = useViewportSize();
+    const form = useForm({
+        initialValues: {
+            title: 'ABC',
+            content: 'rolex',
+        },
+        validate: {
+            title: (value) => value,
+            content: (value) => value
+        },
+    });
+    return (
+        <Grid>
+            <Grid.Col xl={6} lg={6} md={6} sm={6} xs={12}>
+                <MediaQuery
+                    query="(max-width: 1800px) and (min-width: 900px)"
+                    styles={{
+                        border: "1px solid #f1f1f1",
+                    }}
+                >
+                    <Container className="detail-image-container">
+                        <Image
+                            src="https://bossluxurywatch.vn/uploads/san-pham/rolex/sky-dweller/rolex-sky-dweller-42mm-326938-0005.png"
+                            alt="watch"
+                            height="320px"
+                            fit="contain"
+                        />
+                    </Container>
+                </MediaQuery>
+                <Group position="left" mt="md">
+                    <Button variant="outline" color="dark">Chọn ảnh</Button>
+                </Group>
+            </Grid.Col>
+            <Grid.Col xl={6} lg={6} md={6} sm={6} xs={12}>
+
+                <Box >
+                    <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                        <TextInput
+                            required
+                            label="Title"
+                            placeholder="Input title"
+                            {...form.getInputProps('title')}
+                        />
+                        <Textarea
+                            label="Content"
+                            placeholder="Input content"
+                            autosize
+                            minRows={10}
+                            maxRows={10}
+                            {...form.getInputProps('content')}
+                        />
+                        <Group position="right" mt="md">
+                            <Button type="submit" color="green">Thêm sản phẩm</Button>
+                        </Group>
+                    </form>
+                </Box>
+            </Grid.Col>
+        </Grid>
+    );
 }
--- Client/src/component/general/sidebar.js
+++ Client/src/component/general/sidebar.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { Stack, Button, Title, Affix, Anchor } from '@mantine/core';
+import { Stack, Button, Title, Anchor, Grid } from '@mantine/core';
 import { Power } from "tabler-icons-react";
+import Logo from "../general/logo";
 import '../../css/sidebar.css'
 
 
 export default function SideBar({ selected }) {
     return (
-        <Stack className="sidebar" justify="flex-start" spacing="xl" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0], height: 300 })}>
-            <Title order={1} className="sidebar__title">BKlock</Title>
-            <Anchor href="/admin/products"><Button variant="subtle" className={selected === 0 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Product</Button></Anchor>
-            <Anchor href="/admin/news"><Button variant="subtle" className={selected === 1 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> News </Button></Anchor>
-            <Anchor href="/admin/member"><Button variant="subtle" className={selected === 2 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Member</Button></Anchor>
-            <Button variant="subtle" className="sidebar__item--out"><Power />Log out</Button>
+        <Stack className="sidebar" maxHeight justify="space-between" spacing="xl" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0], height: 300 })}>
+            <Stack className="sidebar__part">
+                {/* <Title order={1} className="sidebar__title">BKlock</Title> */}
+                <Logo classname={"sidebar__logo"}/>
+                <Anchor href="/admin"><Button variant="subtle" className={selected === 0 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý sản phẩm</Button></Anchor>
+                <Anchor href="/admin/news"><Button variant="subtle" className={selected === 1 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý tin tức </Button></Anchor>
+                <Anchor href="/admin/member"><Button variant="subtle" className={selected === 2 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý thành viên</Button></Anchor>
+            </Stack>
+            <Button variant="subtle" className="sidebar__item sidebar__item--out sidebar__part"><Power />Log out</Button>
         </Stack>
     );
 }
