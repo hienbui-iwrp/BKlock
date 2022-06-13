ba56b6ec032212bcc402eda3a76d1636aee4029f
Bùi Lương Vinh Hiển <hien.bui250801@gmail.com>
2022-06-13 22:39:53 +0700

    css

--- Client/src/component/general/sidebar.js
+++ Client/src/component/general/sidebar.js
@@ -12,7 +12,7 @@ export default function SideBar({ selected }) {
         <Stack className="sidebar" maxHeight justify="space-between" spacing="xl" sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0], height: 300 })}>
             <Stack className="sidebar__part">
                 {/* <Title order={1} className="sidebar__title">BKlock</Title> */}
-                <Button color="dark" radius="xs" uppercase style={{height:80}}>
+                <Button color="dark" radius="xs" uppercase style={{ height: 80 }}>
                     <Text
                         weight={700}
                         style={{
@@ -26,12 +26,12 @@ export default function SideBar({ selected }) {
                         lock
                     </Text>
                 </Button>
-                <Anchor href="/admin"><Button variant="subtle" className={selected === 0 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý sản phẩm</Button></Anchor>
+                <Anchor href="/admin" ><Button variant="subtle" className={selected === 0 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý sản phẩm</Button></Anchor>
                 <Anchor href="/admin/news"><Button variant="subtle" className={selected === 1 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý tin tức </Button></Anchor>
-                <Anchor href="/admin/member"><Button variant="subtle" className={selected === 2 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý thành viên</Button></Anchor>
+                <Anchor href="/admin/member" ><Button variant="subtle" className={selected === 2 ? "sidebar__item--selected sidebar__item" : "sidebar__item"}> Quản lý thành viên</Button></Anchor>
 
             </Stack>
-            <Button variant="subtle" className="sidebar__item sidebar__item--out sidebar__part"><Power />Log out</Button>
+            <Anchor href="/login" className="sidebar__item sidebar__item--out sidebar__part"><Power />Log out</Anchor>
         </Stack>
     );
 }
